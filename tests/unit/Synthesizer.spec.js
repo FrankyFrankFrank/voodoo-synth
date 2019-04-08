0bccdde8142443f84f56972cfcd3ff38306ac1dc
Adam Frank <a.frank@vehikl.com>
2019-04-07 23:06:26 -0400

    test triggers method on keypress C

--- tests/unit/Synthesizer.spec.js
+++ tests/unit/Synthesizer.spec.js
@@ -6,10 +6,29 @@ describe('Synthesizer', () => {
   it('renders', () => {
     const wrapper = shallowMount(Synthesizer, {
       propsData: {
-        audioContext: AudioContext
-      }
+        audioContext: AudioContext,
+      },
     });
 
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it('plays a C note when the c key pressed', () => {
+    const playNoteStub = jest.fn();
+    const wrapper = shallowMount(Synthesizer, {
+      propsData: {
+        audioContext: AudioContext
+      },
+      attachToDocument: true,
+      methods: {
+        playNote: playNoteStub,
+      }
+    });
+
+    wrapper.trigger('keydown', {
+      key: 'c'
+    });
+
+    expect(playNoteStub).toHaveBeenCalledWith(expect.objectContaining({ key: 'c' }));
   });
 });
