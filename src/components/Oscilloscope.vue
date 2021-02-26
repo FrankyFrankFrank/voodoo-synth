218c39e550b2044bf701fc250bb6eb5cae987c22
nick <nick@nickdown.com>
2021-02-26 13:12:34 -0500

    Refactor oscilloscope

--- src/components/Oscilloscope.vue
+++ src/components/Oscilloscope.vue
@@ -5,21 +5,64 @@
 </template>
 
 <script>
-import OscilloscopeFoo from 'oscilloscope'
+
+function drawScope(analyser, ctx) {
+  // Copyright (c) 2021 by Ewen (https://codepen.io/ContemporaryInsanity/pen/Mwvqpb)
+  // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+  var width = ctx.canvas.width;
+  var height = ctx.canvas.height;
+  var timeData = new Uint8Array(analyser.frequencyBinCount);
+  var scaling = height / 256;
+  var risingEdge = 0;
+  var edgeThreshold = 5;
+
+  analyser.getByteTimeDomainData(timeData);
+
+  ctx.fillStyle = 'transparent';
+  ctx.clearRect(0, 0, width, height);
+
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = 'rgb(0, 200, 0)';
+  ctx.beginPath();
+
+  // No buffer overrun protection
+  while (timeData[risingEdge++] - 128 > 0 && risingEdge <= width);
+  if (risingEdge >= width) risingEdge = 0;
+
+  while (timeData[risingEdge++] - 128 < edgeThreshold && risingEdge <= width);
+  if (risingEdge >= width) risingEdge = 0;
+
+  for (var x = risingEdge; x < timeData.length && x - risingEdge < width; x++)
+    ctx.lineTo(x - risingEdge, height - timeData[x] * scaling);
+
+  ctx.stroke();
+}
 
 export default {
   name: "Oscilloscope",
   props: ['signal'],
+  data() {
+    return {
+      context: null,
+      canvas: null
+    }
+  },
   mounted() {
-    const scope = new OscilloscopeFoo(this.signal, {fftSize: 2048})
+    this.canvas = document.getElementById("c");
+    this.canvas.width = window.innerWidth
+    this.canvas.height = window.innerHeight / 4
+    this.context = this.canvas.getContext("2d")
 
-    let canvas = document.getElementById("c");
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight / 4
-    let context = canvas.getContext("2d")
-
-    context.strokeStyle = "white"
-    scope.animate(context)
+    this.oscillatorLoop()
+  },
+  methods: {
+    oscillatorLoop() {
+      drawScope(this.signal, this.context);
+      requestAnimationFrame(this.oscillatorLoop);
+    }
   }
 }
 </script>
